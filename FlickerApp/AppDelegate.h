6018b7d0f1f5c6b640edfab588af2278a3fadb9a
Rincewind <rincewind@Rincewinds-Mac-mini.local>
2013-01-17 13:55:07 +0100

    Initial Commit

---
+++ FlickerApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FlickerApp
+//
+//  Created by Rincewind on 17.01.13.
+//  Copyright (c) 2013 Rincewind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FlickerApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FlickerApp
+//
+//  Created by Rincewind on 17.01.13.
+//  Copyright (c) 2013 Rincewind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
